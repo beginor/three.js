6b6d8bac1d5ab55edb21d6f1e1cbec25de072237
Yannis Gravezas <wizgrav@gmail.com>
2020-02-13 13:08:53 +0000

    lint

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -980,7 +980,7 @@ function WebGLRenderer( parameters ) {
 
 		currentRenderState = renderStates.get( scene, camera );
 		currentRenderState.init();
-		
+
 		_projScreenMatrix.multiplyMatrices( camera.projectionMatrix, camera.matrixWorldInverse );
 		_frustum.setFromMatrix( _projScreenMatrix );
 
