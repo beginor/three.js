801c645e23156375a6ee407f6aa42e0ec6848b62
Jacob Bijani <jacob.bijani@gmail.com>
2020-05-15 17:27:19 -0400

    explicitly check for undefined, and remove debug log

--- examples/jsm/loaders/FBXLoader.js
+++ examples/jsm/loaders/FBXLoader.js
@@ -2537,9 +2537,8 @@ var FBXLoader = ( function () {
 									if ( modelID !== undefined ) {
 
 										var rawModel = fbxTree.Objects.Model[ modelID.toString() ];
-										
-										if (!rawModel) {
-											console.log("invalid rawModel", modelID, child);
+
+										if ( rawModel === undefined ) {
 											return;
 										}
 
